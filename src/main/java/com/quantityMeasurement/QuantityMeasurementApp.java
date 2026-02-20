3ba65ba6847eff4aea050724e629174ee86c1760
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-02-20 16:52:00 +0530

    [Harsh Raj Singh] : modified main class for Inch Equality

--- src/main/java/com/quantityMeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/quantityMeasurement/QuantityMeasurementApp.java
@@ -5,6 +5,12 @@ public class QuantityMeasurementApp {
 		Feet feet1=new Feet(5.0);
 		Feet feet2=new Feet(5.0);
 		
-		System.out.println("Equality :"+ feet1.equals(feet2));
+		System.out.println("Feet Equality : "+ feet1.equals(feet2));
+		
+		Inch inch1 = new Inch(1.0);
+		Inch inch2 = new Inch(1.0);
+		
+		System.out.println("Inch Equality : "+ inch1.equals(inch2));
+		
 	}
 }
